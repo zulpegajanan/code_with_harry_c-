e661a4d7fdc9d7c2e6c0526448a0d791552a3121
Zulpe Gajanan <91510263+zulpegajanan@users.noreply.github.com>
2022-07-04 13:25:54 +0530

    Create 8_useOf_setw.cpp

---
+++ 8_useOf_setw.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include<iomanip>
+int main()
+{
+    int rakesh=1234;
+    cout<<"MANUPLULATOR IN C++\n endl\nsetw\n etc"<<endl;
+    cout<<"\nwithout setw \n"<<rakesh<<endl;
+    cout<<"with setw(wright supitution) \n"<<setw(8)<<rakesh<<endl;
+    
+    
+    cout<<"\nCONSTANT C VARIABLE IN C++"<<endl;
+    const int gaju=21;
+    //gaju=44;//this will produce ERROR .
+   return 0;
+}
