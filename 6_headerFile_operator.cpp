c6230e291e552752a851121ffc3460cd1eba59fa
Zulpe Gajanan <91510263+zulpegajanan@users.noreply.github.com>
2022-07-04 12:39:04 +0530

    Create 6_headerFile_operator.cpp

---
+++ 6_headerFile_operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //there are two type of header file
+  //1.system headerfile
+  //2.userdefine headerfile
+  cout<<"there are two type of header file\n 1.system headerfile\n 2.userdefine headerfile" ;
+
+    cout<<"\n\nARITHMATIIC OPERATOR \n +\n -\n /\n *\n %\n ++\n --";
+    
+    cout<<"\n\nCOMPARISON OPERATOR \n ==\n !=\n /\n <=\n >=\n <\n >\n --";
+    
+    return 0;
+}
