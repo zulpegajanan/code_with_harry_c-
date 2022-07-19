331ec749c72c968ee7631f1b7c0f1a52bf43b9b2
Zulpe Gajanan <91510263+zulpegajanan@users.noreply.github.com>
2022-07-19 17:59:27 +0530

    Create 95_VECTOR_OF_VECTOR.cpp

---
+++ 95_VECTOR_OF_VECTOR.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+  
+int main()
+{
+    vector<vector<int>> vect
+    {
+        { 2, 3},
+        {4, 5, 6},
+        {7, 8, 9,0},
+        {99}
+    };
+      
+    for (int i = 0; i < vect.size(); i++)
+    {
+        for (int j = 0; j < vect[i].size(); j++)
+        {
+            cout << vect[i][j] << " ";
+        }    
+        cout << endl;
+    }
+  
+    return 0;
+}
